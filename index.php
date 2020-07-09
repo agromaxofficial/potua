6472cfef373b0548148ea895a7b35b60312e13d5
sheggy777 <xxx.jaycpi@gmail.com>
2020-07-09 19:25:57 +0400

    1

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="js/jquery.min.js"></script>	<meta charset="utf-8">
+	<meta name="viewport" content="width=480">
+	<title>Интернет-магазин приманки для рыбалки.</title>
+    <meta name="keywords" content="Приманка рыболовная, приманка, приманка купить, приманка заказать">
+    <meta name="description" content="Универсальная приманка для рыбалки. Подходит для приваживания рыбы в любое время года.">
+	<link rel="shortcut icon" href="favicon.ico"> 
+	<link rel="stylesheet" href="css/settings.css">
+	<link rel="stylesheet" href="css/slick.css">
+	<link rel="stylesheet" href="css/timer.css">
+	<link rel="stylesheet" href="css/style.css">
+		<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(65396212, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/65396212" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+</head>
+<body>
+<!-- after_qbici -->
+<div class="wrap">
+    <section class="b1">
+        <h1>Fish-Нunt - активатор клева</h1>
+        <h2>Приманка с феромонами для рыбалки</h2>
+        <div class="sale">Скидка - 72%</div>
+        <ul>
+            <li>Смело умножай свой обычный улов на 4!</li>
+            <li>Любимая смесь запахов для ВСЕХ видов рыб</li>
+            <li>Окупается уже в первый выезд на рыбалку</li>
+        </ul>
+    </section>
+    <!-- michael-k90 -->
+    <section class="b2">
+        <div class="price clearfix">
+            <span class="old-cost">830грн</span>
+            <span class="new-cost">69грн</span>
+        </div>
+        <a href="#buy" class="button-m">Заказать со скидкой</a>
+    </section>
+    <!-- owmen17 -->
+    <section class="b4">
+        <h2 class="title">ты будешь думать не о том,<br> как поймать рыбу, а о том, как довезти улов домой!</h2>
+        <p>Fish-Нunt это мощная формула, компоненты которой работают в комплексе, усиливая эффект друг друга. На данный момент средство для приваживания рыбы по своей эффективности не имеет ни одного аналога</p>
+    </section>
+    <!-- after_qbici -->
+    <section class="b5">
+        <ul class="secret">
+            <li><h3>Альбумин</h3> состоит на 95% из чистого протеина – наиболее быстро усваиваемого источника питания. Рыбы способны распознавать пищу, поедание которой наиболее эффективно, и отдавать ей предпочтение</li>
+            <li><h3>Синтезированные феромоны</h3> вызывают у рыбы чувство неконтролируемого голода, и она двигается к точке, в которой феромон попал в воду. В этом месте рыба бросается на любую наживку, что обеспечит вам высокий улов</li>
+        </ul>
+    </section>
+    <!-- endzeit_____ -->
+    <section class="b6">
+        <h2 class="title">Мнение профессионала</h2>
+        <h3>Юрий Сипцов, чемпион Европы по ловле рыбы</h3>
+        <p>Преимущество этой приманки заключается в том, что она одинаково хорошо действует как на мирную рыбу, так и на хищную. Говоря с научной точки зрения, синтезированные феромоны вызывают безудержное чувство голода - и каждая рыба начинает поиски возбудителя. Поймать такую остается лишь делом техники. Именно поэтому эта приманка стала настолько популярна в Европе. Рыбачить с ней действительно легко и приятно!</p>
+    </section>
+    <!-- michael-k90 -->
+    <section class="b7">
+        <h2 class="title">Успейте заказать Fish-Нunt  сегодня со скидкой -72%</h2>
+        <ul class="magnet">
+            <li>Единственный активатор клева, испытанный зимой и летом в реальных условиях - реках, озерах, морях и др. </li>
+            <li>Наиболее эффективна для приваживания карася, карпа, плотвы, линя, а также щуки, окуня, ерша, сома, судака, налима</li>
+            <li>90% наших покупателей заказывают приманку снова и снова. Рыбакам действительно нравятся огромные уловы!</li>
+        </ul>
+        <a href="#buy" class="button-m">Заказать со скидкой</a>
+    </section> 
+    <!-- bizlife_inc -->
+    <section class="b8">
+        <h2 class="title">Как использовать?</h2>
+        <p>Пользоваться приманкой очень просто. Fish-Нunt добавляется в прикормку, Вы можете сделать её самостоятельно или купить в рыболовном магазине</p>
+        <ul class="use">
+            <li>
+                <h3>Для ловли мирной рыбы</h3>
+                <p>Смешать 1 пакет с 1 кг любой прикормки. Для удобства можно растворить Fish-Нunt в небольшом количестве воды</p>
+            </li>
+            <li>
+                <h3>Для ловли хищной рыбы</h3>
+                <p>Обработать все искусственные и естественные приманки. Силиконовые приманки, воблеры или блесна можно опустить в емкость с аттрактантом</p>
+            </li>
+            <li>
+                <h3>С живой наживкой</h3>
+                <p>Для этого рекомендуется растворить содержимое 2-х пакетов в 0,5 л любой воды и окунуть туда наживку перед насадкой</p>
+            </li>
+        </ul>
+    </section>
+    <!-- owmen17 -->
+    <section class="b9">
+        <h2 class="title">Отзывы покупателей</h2>
+        <div class="reviews">
+            <div class="rev">
+                <h3>Алексей Суровцев, г. Кривой Рог</h3>
+                <p>Никогда бы не подумал, что смогу наловить 15 килограммов карасей за раз)) И приехал-то в глушь, где рыба никогда особо не жаловала. Перемешал этот активатор c самодельным прикормом, забросил - и весь следующий час даже не садился) Рыба клюет, как бешеная! Мужики, заказывайте смело!!! </p>
+            </div>
+            <div class="rev r2">
+                <h3>Максим Колесников, г. Киев</h3>
+                <p>Если честно, сомневался, боялся заказывать) Но теперь сомнений нет - этот активатор действительно работает! Недавно съездил на щуку и убедился в этом лично. 18 щучек заловил! Таких голодных никогда не видел - от феромонов они чуть ли из воды не прыгают. Удивительно))</p>
+            </div>
+            <div class="rev r3">
+                <h3>Иван Успенский, г. Львов</h3>
+                <p>С тех пор как стал брать эту приманку с собой, жена на рыбу смотреть не может)) Ловится и вправду очень много. Даже там, где улов уже был примерно известен, выходит раза в три больше. Отличная вещь, главное запаситесь терпением - чистить придется много))</p>
+            </div>
+        </div>
+        <br>
+        <h2 class="title">Видео покупателей</h2>
+        <div class="video-container">
+            <div class="youtube" id="mjlwPxlGWTE"></div>
+        </div> <br>
+        <div class="video-container">
+            <div class="youtube" id="OmJmNOOxLgw"></div>
+        </div> <br>
+        <div class="video-container">
+            <div class="youtube" id="-S6u5HZvMjI"></div>
+        </div>
+    </section>
+    <!-- after_qbici -->
+    <section class="b10">
+        <h2 class="title">Как заказать Fish-Нunt?</h2>
+        <ul class="ship">
+            <li><h3>Оформление</h3> Оставьте заявку на нашем сайте и мы свяжемся с Вами для оформления заказа</li>
+            <li><h3>Доставка</h3> Действует быстрая доставка по Украине в любой регион. Время доставки от 2 до 5 дней</li>
+            <li><h3>Оплата</h3> Никакой предоплаты! Вы оплачиваете заказ только при получении после проверки</li>
+            <li><h3>Гарантии</h3> Вы вправе вернуть товар в течение 14 дней с момента получения</li>
+        </ul>
+    </section>
+    <section class="b11">
+        <h1>Fish-Нunt - активатор клева</h1>
+        <h2>РАСПРОДАЖА ТОЛЬКО ОДИН ДЕНЬ  !</h2>
+        <div class="sale">Скидка - 72%</div>
+        <div class="price clearfix">
+            <span class="old-cost">830грн</span>
+            <span class="new-cost">69грн</span>
+        </div>
+    </section>
+    <section class="b12">
+        <h2><span>Успейте заказать со скидкой!</span> Халява закончится через:</h2>    
+        <div class="countbox"></div>
+        <form id="buy" class="main-order-form m1-form" action="" method="post">
+            <input class="field" name="name" type="text" placeholder="Введите ваше имя" required>
+            <input class="field" name="phone" type="tel" placeholder="Введите ваш телефон" required>
+            <button class="button-m">Заказать со скидкой</button>
+</form>
+    </section>
+    <!-- endzeit_____ -->            
+    <footer class="footer">
+            <img src="images/black-320.png" alt="copyright">   
+            <p style="text-align: center">
+                <a href="politic.html" target="_blank">Политика конфиденциальности</a>
+                <a href="oferta.html" target="_blank">Пользовательское соглашение</a>
+            </p>
+    </footer>
+</div>
+
+    <script type="text/javascript">
+        var phonePrefix = '+380';
+    </script>
+    <script type="text/javascript" src="js/phonePrefix.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-url-parser/2.3.1/purl.min.js"></script>
+    <script src="https://click.lucky.online/js/leadprofit.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/jquery.inputmask.bundle.min.js"></script>
+
+    <script>
+        $("form").leadprofit({
+            hash: "11e7baf1-962c-4328-a53a-7e4ff6827764"
+        }).leadprofitSubmit({
+            confirmUrl: 'confirm.html',
+            counterParams: {
+                country: "{country}",
+                version: document.location.pathname.replace(/\/+/g, '').replace('landings', '').replace('index.html', '').replace('index2.html', '').replace('confirm.html', '')
+            }
+        });
+    </script>
+
+<script src="price.js"></script>
+
+<script src="js/html5shiv.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/count.js"></script>
+<script src="js/main.js"></script>
+<script src="js/previewYouTube.js"></script>
+<!-- endzeit_____ -->
+
+	<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1022120668204642');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1022120668204642&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+</body>
+</html>
