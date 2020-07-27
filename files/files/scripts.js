3a8d678d6dd8306e19dbfad26a1e335a0d79b90e
sheggy777 <xxx.jaycpi@gmail.com>
2020-07-27 13:21:01 +0400

    1

--- files/files/scripts.js
+++ files/files/scripts.js
@@ -1,42 +1,72 @@
 $(document).ready(function() {
+	
+	/* scroll */
+	
+	$("a[href^='#']").click(function(){
+		var _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
 
-     /* lazy load */
-    var blazy = new Blazy();
+	/* timer */
 
-    
-    /* scroll */
-    $("a[href^='#']").click(function(){
-        var _href = $(this).attr("href");
-        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-        return false;
-    });
+	function update() {
+		var Now = new Date(), Finish = new Date();
+		Finish.setHours( 23);
+		Finish.setMinutes( 59);
+		Finish.setSeconds( 59);
+		if( Now.getHours() === 23  &&  Now.getMinutes() === 59  &&  Now.getSeconds === 59) {
+			Finish.setDate( Finish.getDate() + 1);
+		}
+		var sec = Math.floor( ( Finish.getTime() - Now.getTime()) / 1000);
+		var hrs = Math.floor( sec / 3600);
+		sec -= hrs * 3600;
+		var min = Math.floor( sec / 60);
+		sec -= min * 60;
+		$(".timer .hours").html( pad(hrs));
+		$(".timer .minutes").html( pad(min));
+		$(".timer .seconds").html( pad(sec));
+		setTimeout( update, 200);
+	}
+	function pad(s) {
+		s = ("00"+s).substr(-2);
+		return "<span>" + s[0] + "</span><span>" + s[1] + "</span>";
+	}
+	update();
 
-    /* timer */
-    function update() {
-        var Now = new Date(), Finish = new Date();
-        Finish.setHours( 23);
-        Finish.setMinutes( 59);
-        Finish.setSeconds( 59);
-        if( Now.getHours() === 23  &&  Now.getMinutes() === 59  &&  Now.getSeconds === 59) {
-            Finish.setDate( Finish.getDate() + 1);
-        }
-        var sec = Math.floor( ( Finish.getTime() - Now.getTime()) / 1000);
-        var hrs = Math.floor( sec / 3600);
-        sec -= hrs * 3600;
-        var min = Math.floor( sec / 60);
-        sec -= min * 60;
-        $(".timer .hours").text( pad(hrs));
-        $(".timer .minutes").text( pad(min));
-        $(".timer .seconds").text( pad(sec));
-        setTimeout( update, 200);
-    }
-    function pad(s) { return ('00'+s).substr(-2) }
-    update();
+	/* sliders */
 
-    $('.op .button-m').click(function() {
-        var data = $(this).attr('data-item');
-        console.log(data);
-        $('.item').val('Товар: ' + data);
-    });
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		smartSpeed: 300,
+		mouseDrag: false,
+		pullDrag: false,
+		dots: false,
+		nav: true,
+		navText: ""
+	});
+
+
+	/* validate form */
+
+	$(".order_form").submit(function(){
+		if ($(this).find("input[name='name']").val() == "" && $(this).find("input[name='phone']").val() == "") {
+			alert("Введите Ваши имя и телефон");
+			$(this).find("input[name='name']").focus();
+			return false;
+		}
+		else if ($(this).find("input[name='name']").val() == "") {
+			alert("Введите Ваше имя");
+			$(this).find("input[name='name']").focus();
+			return false;
+		}
+		else if ($(this).find("input[name='phone']").val() == "") {
+			alert("Введите Ваш телефон");
+			$(this).find("input[name='phone']").focus();
+			return false;
+		}
+		return true;
+	});
 
 });
